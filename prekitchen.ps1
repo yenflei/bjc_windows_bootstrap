eb2973aec91f0a6bbe10fc29a8c8c42e84fb753e
tiobagio <tbagio@gmail.com>
2018-11-17 15:22:18 +0800

    modify prekitchen.ps1

--- prekitchen.ps1
+++ prekitchen.ps1
@@ -1,19 +1,24 @@
+# README:
+# This utility is run before `kitchen create`.  It populates .kitchen.local.yml with the subnet_id and security_group_id of the instance where this utility runs.
+#
+# Need some checking like this (taken from AWS EC2 tool)
+#
 #x=$(curl -s http://169.254.169.254/)
 #if ( $? -gt 0 ) {
 #        Write-Host '[ERROR] Command not valid outside EC2 instance. Please run this command within a running EC2 instance.'
 #        exit 1
 #}
 
-$mac = (Invoke-WebRequest -Uri http://169.254.169.254/latest/meta-data/mac -UseBasicParsing).Content
-$sg = (Invoke-WebRequest -Uri http://169.254.169.254/latest/meta-data/network/interfaces/macs/$mac/security-group-ids -UseBasicParsing).Content
-$subnet = (Invoke-WebRequest -Uri http://169.254.169.254/latest/meta-data/network/interfaces/macs/$mac/subnet-id -UseBasicParsing).Content
+$mymac = (Invoke-WebRequest -Uri http://169.254.169.254/latest/meta-data/mac -UseBasicParsing).Content
+$mysg = (Invoke-WebRequest -Uri http://169.254.169.254/latest/meta-data/network/interfaces/macs/$mymac/security-group-ids -UseBasicParsing).Content
+$mysubnet = (Invoke-WebRequest -Uri http://169.254.169.254/latest/meta-data/network/interfaces/macs/$mymac/subnet-id -UseBasicParsing).Content
 
-Write-Host "writing to .kithcen.local.yml with $sg on subnet $subnet_id"
+Write-Host "writing to .kitchen.local.yml with security group $mysg on subnet $mysubnet"
 
 $entries = "
 ---
 driver:
-    security-group-ids: $sg
-    subnet-id: $subnet
-"    
+    security_group_ids: $mysg
+    subnet_id: $mysubnet
+"
 Set-Content -Path .\\.kitchen.local.yml $entries
